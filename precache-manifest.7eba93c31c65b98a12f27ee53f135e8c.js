736010268ad5dd39469047508e3883b05444c830
legbalimited <legbalimited@gmail.com>
2021-12-13 15:55:55 -0500

    v1

---
+++ precache-manifest.7eba93c31c65b98a12f27ee53f135e8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa686da0818ed3502e29ba600411413c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304184583d9850cd17ff",
+    "url": "/static/css/main.9b3ac610.chunk.css"
+  },
+  {
+    "revision": "2b91a7709c6a2b212d5c",
+    "url": "/static/js/2.b845edf2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.b845edf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304184583d9850cd17ff",
+    "url": "/static/js/main.567fbf7e.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "53ae2b197adfc5b70c4f35182e0c6a4b",
+    "url": "/static/media/cloud-architecture.53ae2b19.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "954efe72cdc4a43abbd1fb928fe8886b",
+    "url": "/static/media/dashboard.954efe72.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
